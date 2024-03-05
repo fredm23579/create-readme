70a89856280e7044e27a2015334fece166736585
fredm23579 <motta@g.ucla.edu>
2024-03-04 17:09:58 -0800

    Placed triple bactick symbols for install git clone command for bash

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
+let clone = "```";
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   const badges = {
@@ -48,7 +49,10 @@ ${data.description}
 * [Questions](#questions)
 
 ## Installation
+
+${clone}
 ${data.installation}
+${clone}
 
 ## Usage
 ${data.usage}
